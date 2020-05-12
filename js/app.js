9ee6d64a1be4922468b18974b842582cfb741b18
dustin <dustinusey@me.com>
2020-05-12 16:36:23 -0500

    initial commit
    file dump

---
+++ js/app.js
@@ -0,0 +1,50 @@
+/* Generate Project Cards from data.js */
+
+const projectContainer = document.getElementById('project-container');
+proj.forEach((project, index) => {
+	projectContainer.innerHTML += `
+	<div class="project-card">
+		<div class="tech-used">
+		</div>
+		<div class="card-info">
+			<p class="time">${proj[index].timeToComplete}</p>
+			<h3>${proj[index].name}</h3>
+			<p>Creator: ${proj[index].creator}</p>
+			<p>${proj[index].date}</p>
+			<h4>Description</h4>
+			<p class="desc">${proj[index].description}</p>
+		</div>
+		<div class="card-buttons">
+			<a target="_blank" href="${proj[index].readme}">Instructions</a>
+			<a target="_blank" href="${proj[index].download}">Download</a>
+		</div>
+	</div>
+`
+	const techUsed = document.querySelectorAll('.tech-used');
+	if (proj[index].html === true) {
+		techUsed[index].innerHTML += `<div class="html-tech"></div>`
+	}
+	if (proj[index].css === true) {
+		techUsed[index].innerHTML += `<div class="css-tech"></div>`
+	}
+	if (proj[index].javascript === true) {
+		techUsed[index].innerHTML += `<div class="js-tech"></div>`
+	}
+});
+
+
+
+/* Search functionality */
+
+const search = document.getElementById('project-search');
+const project = projectContainer.querySelectorAll('.project-card')
+search.addEventListener('keyup', () => {
+	let query = search.value.toLowerCase();
+	proj.forEach((tag, index) => {
+		if (tag.tags.includes(query)) {
+			project[index].style.display = 'grid';
+		} else {
+			project[index].style.display = 'none';
+		}
+	});
+});
---
+++ js/data.js
@@ -0,0 +1,28 @@
+const proj = [
+	{
+		name: 'DOM Manipulation Practice App',
+		creator: 'Jamie Reardon',
+		date: 'Thursday May 5, 2020',
+		description: 'The project is based on the beloved Treehouse landing page for the techdegree. The page contains three widgets that you will manipulate with JavaScript to complete the functionlity to the page.',
+		tags: 'expert jamie dom manipulation practice app javascript treehouse landing page widget',
+		readme: 'https://github.com/jmereardon17/techdegree-slack-challenges-project-01/blob/master/README.md',
+		download: 'https://github.com/jmereardon17/techdegree-slack-challenges-project-01/archive/master.zip',
+		timeToComplete: '2 hours',
+		html: false,
+		css: false,
+		javascript: true
+	},
+	{
+		name: 'From Mockup to Code',
+		creator: 'Dustin Usey',
+		date: 'Wednesday May 13, 2020',
+		description: "You're working for a company as a front-end dev and your boss tells you that a project your team has been working on is near completion! They need you to create the landing age from a mockup the design team has created using HTML & CSS.",
+		tags: 'beginner dustin from mockup to code html css plugin frontend landing',
+		readme: 'https://github.com/dustinusey/firebasejs',
+		download: 'https://github.com/dustinusey/firebasejs/archive/master.zip',
+		timeToComplete: '2 hours',
+		html: true,
+		css: true,
+		javascript: false
+	}
+]
